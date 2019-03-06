e8c15ab1c26fe6c0df5f83d5f3aae90c43537eec
Matthew Fenlon <matthewfenlon@gmail.com>
2019-03-06 10:05:40 -0500

    filter for commas in archives title. cleanup and simplification of code.

--- includes/filter-commas-in-taxonomy.php
+++ includes/filter-commas-in-taxonomy.php
@@ -9,32 +9,45 @@
  * Follow me on Twitter: @HertogJanR
  */
 
-// filter for tags (as a taxonomy) with comma
+//Allow substitution of -- for comma in all taxonomies
 //  replace '--' with ', ' in the output - allow tags with comma this way
 if( !is_admin() ) { // make sure the filters are only called in the frontend
-    //Allow substitution of -- for comma in all taxonomies
-    //$custom_taxonomy_type = 'location'; // here goes your taxonomy type
 
-    function comma_taxonomy_filter( $tag_arr ){
-        global $custom_taxonomy_type;
-        $tag_arr_new = $tag_arr;
-        //if( $tag_arr->taxonomy == $custom_taxonomy_type && strpos( $tag_arr->name, '--' ) ){
-        if( strpos( $tag_arr->name, '--' ) ){
+    //Filter when taxonomy is displayed as items
+    if ( ! function_exists( 'ripm_jcpt_comma_taxonomies_filter' ) ) {
 
-                $tag_arr_new->name = str_replace( '--' , ', ', $tag_arr->name);
+        add_filter('get_the_taxonomies', 'ripm_jcpt_comma_taxonomies_filter');
+        add_filter('get_terms', 'ripm_jcpt_comma_taxonomies_filter');
+        add_filter('get_the_terms', 'ripm_jcpt_comma_taxonomies_filter');
+
+        function ripm_jcpt_comma_taxonomies_filter($tags_arr) {
+            $tags_arr_new = array();
+            foreach ($tags_arr as $tag_arr) {
+                $tag_arr_new = $tag_arr;
+                $tag_arr_new->name = ripm_jcpt_comma_replace($tag_arr->name);
+                $tags_arr_new[] = $tag_arr_new;
+            }
+            return $tags_arr_new;
         }
-        return $tag_arr_new;
     }
-    add_filter( 'get_custom_taxonomy', 'comma_taxonomy_filter' );
 
-    function comma_taxonomies_filter( $tags_arr ) {
-        $tags_arr_new = array();
-        foreach( $tags_arr as $tag_arr ) {
-            $tags_arr_new[] = comma_taxonomy_filter( $tag_arr );
+    //Filter when taxonomy is displayed as title
+    if ( ! function_exists( 'ripm_jcpt_archive_title' ) ) {
+
+        add_filter('get_the_archive_title', 'ripm_jcpt_archive_title');
+
+        function ripm_jcpt_archive_title($title) {
+            return ripm_jcpt_comma_replace($title);
         }
-        return $tags_arr_new;
     }
-    add_filter( 'get_the_taxonomies', 'comma_taxonomies_filter' );
-    add_filter( 'get_terms', 'comma_taxonomies_filter' );
-    add_filter( 'get_the_terms', 'comma_taxonomies_filter' );
+
+    //Replace -- with a comma
+    if ( ! function_exists( 'ripm_jcpt_comma_replace' ) ) {
+
+        function ripm_jcpt_comma_replace($term) {
+            return str_replace('--', ', ', $term);
+        }
+    }
+
+
 }
